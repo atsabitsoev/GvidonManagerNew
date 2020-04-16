1aa0069f01e010457da7966b3312c4110039fdae
Ацамаз Бицоев <atsamazbitsoev@yandex.ru>
2020-04-16 18:55:34 +0300

    Initial Commit

---
+++ GvidonManagerNew/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GvidonManagerNew
+//
+//  Created by Ацамаз Бицоев on 16.04.2020.
+//  Copyright © 2020 Ацамаз Бицоев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
