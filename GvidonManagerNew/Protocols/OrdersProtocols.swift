9b699c4ebe6942545fb1d82b6f2ae14f2a63fee1
Ацамаз Бицоев <atsamazbitsoev@yandex.ru>
2020-04-22 17:26:21 +0300

    Добавлены протоколы для View и Controller

---
+++ GvidonManagerNew/Protocols/OrdersProtocols.swift
@@ -0,0 +1,18 @@
+//
+//  OrdersProtocols.swift
+//  GvidonManagerNew
+//
+//  Created by Ацамаз Бицоев on 22.04.2020.
+//  Copyright © 2020 Ацамаз Бицоев. All rights reserved.
+//
+
+import UIKit
+
+
+protocol OrderListView: UIView {
+    
+}
+
+protocol ActualOrdersController: UIViewController {
+    
+}
